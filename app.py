af54fcbc64fe509186c036644b0676b2edeb0d48
davidjbuchanan <davidjbuchanan@gmail.com>
2020-04-02 12:42:20 +0000

    html pages added; app.py and base.html updated accordingly

--- app.py
+++ app.py
@@ -5,8 +5,31 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return render_template("base.html")
+def index():
+    return render_template("index.html")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
+@app.route('/recipes')
+def recipes():
+    return render_template("recipes.html")
+
+
+@app.route('/nutrition')
+def nutrition():
+    return render_template("nutrition.html")
+
+@app.route('/calm')
+def calm():
+    return render_template("calm.html")
+
+@app.route('/play')
+def play():
+    return render_template("play.html")
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
